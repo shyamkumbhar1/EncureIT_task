03be4e668a844a154004faafe824e74f282fb306
shyamkumbhar1 <78096242+shyamkumbhar1@users.noreply.github.com>
2024-04-13 02:44:13 +0530

    Optimize code

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,8 @@ use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 use function PHPUnit\Framework\isTrue;
-use App\Http\Controllers\DemoController;
-use App\Http\Controllers\JsonController;
+use App\Http\Controllers\{DemoController};
 use App\Http\Controllers\EmailController;
-use App\Http\Controllers\ExcelController;
 use App\Http\Controllers\IPTrackerController;
 use App\Http\Controllers\ThirdPartyApiController;
 
@@ -19,19 +17,13 @@ Route::get('/', function () {
 Route::get('/demo', [DemoController::class, 'index']);
 Route::post('/demo', [DemoController::class, 'store']);
 
-
 // Task 2 : Find Location Using IP
 Route::get('/ip', [IPTrackerController::class, 'track']);
 
 
 // Task 3 : Get All ID's and Store to excel File
-// Main Logic
 Route::get('getData',[ThirdPartyApiController::class,'getData']);
 Route::get('exportToExcel',[ThirdPartyApiController::class,'exportToExcel']);
-
-// Helper function ##Handle Json
-Route::get('/parse-json', [JsonController::class, 'parseJson']);
-Route::get('/import-excel', [ExcelController::class, 'importExcel']);
 
 // Task 4 : Send Email
 Route::get('/send-email', [EmailController::class, 'showForm']);
